97386abb41147fdd6f5e2bc16f120375c562ffd6
drtgeorgiana <drtgeorgiana@gmail.com>
2022-04-19 15:48:03 +0300

    corrected addresses. work in progress

--- src/main/java/application/solution/Solution.java
+++ src/main/java/application/solution/Solution.java
@@ -4,6 +4,7 @@ import application.dataset.structure.AbstractLocation;
 import application.dataset.structure.Country;
 import application.dataset.structure.State;
 import application.testData.model.TestObject;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.*;
 
@@ -14,107 +15,158 @@ public class Solution {
 
     public static int getNumberOfCorrectAddressesAfterCorrection(List<TestObject> testObjectList) {
         for (TestObject testObject : testObjectList) {
+            if (testObject.getState().equals("bucharest") && testObject.getCity().equals("bucharest")) {
+                testObject.setState("bucuresti");
+                testObject.setCity("bucuresti");
+            }
             TestObject correctedTestObject = getCorrectAddress(testObject);
-            System.out.println(testObject + "" + correctedTestObject);//afiseaza adresele diferite
-            if (testObject.getCity().equals(correctedTestObject.getCity()) && testObject.getState().equals(correctedTestObject.getState()) && testObject.getCountry().equals(correctedTestObject.getCountry())) {
+            if ((testObject.getCity().equals(correctedTestObject.getCity()) && testObject.getState().equals(correctedTestObject.getState()) || testObject.getCity().equals(correctedTestObject.getState()) && testObject.getState().equals(correctedTestObject.getCity())) // am adaugat aceasta conditie deoarece multe adrese generate de site au stateul inversat cu city
+                    && testObject.getCountry().equals(correctedTestObject.getCountry())) {
                 number++;
+            } else {
+                System.out.println(testObject + "" + correctedTestObject);//afiseaza adresele diferite
             }
         }
         return number;
     }
 
     private static TestObject getCorrectAddress(TestObject testObject) {
-        Map<String, Set<String>> mapWithFieldsValue = new HashMap<>();
-        mapWithFieldsValue.put(STATE, getStatesFromTestObject(testObject));
+        Map<String, Map<String, Set<String>>> mapWithFieldsValue = new HashMap<>();
         mapWithFieldsValue.put(CITY, getCitiesFromTestObject(testObject));
+        mapWithFieldsValue.put(STATE, getStatesFromTestObject(testObject));
         mapWithFieldsValue.put(COUNTRY, getCountriesFromTestObject(testObject));
-
-        List<TestObject> correctedAddressList = getAllPossibleCorrectedForm(mapWithFieldsValue);
-
-        return correctedAddressList.get(0);
+        if (mapWithFieldsValue.get(STATE).isEmpty()) {
+            // add states for founded cities mapWithFieldsValue.put(STATE, getStatesForFoundCities(mapWithFieldsValue.get(CITY)));
+        }
+        return getTheBestCorrectedAddress(testObject, mapWithFieldsValue);
     }
 
-    private static List<TestObject> getAllPossibleCorrectedForm(Map<String, Set<String>> mapWithFieldsValue) {
-        List<TestObject> testObjectList = new ArrayList<>();
-        List<String> cities = new ArrayList<>(mapWithFieldsValue.get(CITY));
-        List<String> states = new ArrayList<>(mapWithFieldsValue.get(STATE));
-        List<String> countries = new ArrayList<>(mapWithFieldsValue.get(COUNTRY));
-        TestObject testObject = new TestObject(null, null, null, null, null, null, null);
-        if (!cities.isEmpty()) {
-            testObject.setCity(cities.get(0));
-        }
-        if (!states.isEmpty()) {
-            testObject.setState(states.get(0));
-        }
-        if (!countries.isEmpty()) {
-            testObject.setCountry(countries.get(0));
-        }
-        testObjectList.add(testObject);
-        return testObjectList;
+    private static TestObject getTheBestCorrectedAddress(TestObject testObject, Map<String, Map<String, Set<String>>> mapWithFieldsValue) {
+        String streetLine = testObject.getStreet();
+        String city = testObject.getStreet();
+        String state = testObject.getStreet();
+        String phoneNumber = testObject.getStreet();
+        String zipCode = testObject.getStreet();
+        String countryCallingCode = testObject.getStreet();
+        String country = testObject.getStreet();
+
+        List<Pair<Integer, TestObject>> scoredCorrectedAddresses = new ArrayList<>();
+        System.out.println(mapWithFieldsValue);
+        return new TestObject(null, "brasov", "brasov", null, null, null, "romania");
     }
 
-    private static Set<String> getCitiesFromTestObject(TestObject testObject) {
-        Set<String> cities = new HashSet<>();
-        addElementInCity(testObject.getCountry(), cities);
-        addElementInCity(testObject.getState(), cities);
-        addElementInCity(testObject.getCity(), cities);
+    private static boolean contains(String string, String substring) {
+        List<String> strings = List.of(string.split(" "));
+        List<String> substrings = List.of(substring.split(" "));
+        for (String substr : substrings) {
+            if (!strings.contains(substr)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+//    private static List<TestObject> getAllPossibleCorrectedForm(Map<String, PriorityQueue<String>> mapWithFieldsValue) {
+//        List<TestObject> testObjectList = new ArrayList<>();
+//        List<String> cities = new ArrayList<>(mapWithFieldsValue.get(CITY));
+//        List<String> states = new ArrayList<>(mapWithFieldsValue.get(STATE));
+//        List<String> countries = new ArrayList<>(mapWithFieldsValue.get(COUNTRY));
+//        TestObject testObject = new TestObject(null, null, null, null, null, null, null);
+//        if (!cities.isEmpty()) {
+//            testObject.setCity(cities.get(0));
+//        }
+//        if (!states.isEmpty()) {
+//            testObject.setState(states.get(0));
+//        }
+//        if (!countries.isEmpty()) {
+//            testObject.setCountry(countries.get(0));
+//        }
+//        testObjectList.add(testObject);
+//        return testObjectList;
+//    }
+//
+//    private static Set<String> getStatesForFoundCities(Set<String> strings) {
+//        Set<String> list = new HashSet<>();
+//        for (String str : strings) {
+//            List<AbstractLocation> abstractLocations = SolutionUtil.multimap.get(str);
+//            for (AbstractLocation abstractLocation : abstractLocations) {
+//                if (abstractLocation instanceof State) {
+//                    list.add(abstractLocation.getName());
+//                }
+//            }
+//        }
+//        return list;
+//    }
+
+    private static Map<String, Set<String>> getCitiesFromTestObject(TestObject testObject) {
+        Map<String, Set<String>> cities = new HashMap<>();
+        addElementInCountry(COUNTRY, testObject.getCity(), cities);
+        addElementInState(STATE, testObject.getCity(), cities);
+        addElementInCity(CITY, testObject.getCity(), cities);
         return cities;
     }
 
-    private static Set<String> getCountriesFromTestObject(TestObject testObject) {
-        Set<String> countries = new HashSet<>();
-        addElementInCountry(testObject.getCountry(), countries);
-        addElementInCountry(testObject.getState(), countries);
-        addElementInCountry(testObject.getCity(), countries);
+    private static Map<String, Set<String>> getCountriesFromTestObject(TestObject testObject) {
+        Map<String, Set<String>> countries = new HashMap<>();
+        addElementInCountry(COUNTRY, testObject.getCountry(), countries);
+        addElementInState(STATE, testObject.getCountry(), countries);
+        addElementInCity(CITY, testObject.getCountry(), countries);
         return countries;
     }
 
-    private static Set<String> getStatesFromTestObject(TestObject testObject) {
-        Set<String> states = new HashSet<>();
-        addElementInState(testObject.getCountry(), states);
-        addElementInState(testObject.getState(), states);
-        addElementInState(testObject.getCity(), states);
+    private static Map<String, Set<String>> getStatesFromTestObject(TestObject testObject) {
+        Map<String, Set<String>> states = new HashMap<>();
+        addElementInCountry(COUNTRY, testObject.getState(), states);
+        addElementInState(STATE, testObject.getState(), states);
+        addElementInCity(CITY, testObject.getState(), states);
         return states;
     }
 
-    private static void addElementInCity(String value, Set<String> set) {
-        List<String> allSubsetsFromValue = getAllSubsetsFromString(value);
+    private static void addElementInCity(String fieldName, String value, Map<String, Set<String>> set) {
+        Set<String> allSubsetsFromValue = getAllSubsetsFromString(value);
+        Set<String> foundLocations = new HashSet<>();
         for (String subsetFromValue : allSubsetsFromValue) {
-            List<AbstractLocation> list = SolutionUtil.multimap.get(subsetFromValue);
+            Set<AbstractLocation> list = new HashSet<>(SolutionUtil.multimap.get(subsetFromValue));
             for (AbstractLocation abstractLocation : list) {
                 if (abstractLocation instanceof State) {
-                    set.add(subsetFromValue);
+                    foundLocations.add(subsetFromValue);
                 }
             }
         }
+        set.put(fieldName, foundLocations);
     }
 
-    private static void addElementInState(String value, Set<String> set) {
-        List<String> allSubsetsFromValue = getAllSubsetsFromString(value);
+    private static void addElementInState(String fieldName, String value, Map<String, Set<String>> set) {
+        Set<String> allSubsetsFromValue = getAllSubsetsFromString(value);
+        Set<String> foundLocations = new HashSet<>();
         for (String subsetFromValue : allSubsetsFromValue) {
-            List<AbstractLocation> list = SolutionUtil.multimap.get(subsetFromValue);
+            Set<AbstractLocation> list = new HashSet<>(SolutionUtil.multimap.get(subsetFromValue));
             for (AbstractLocation abstractLocation : list) {
                 if (abstractLocation instanceof Country) {
-                    set.add(subsetFromValue);
-                } else {
-                    if (abstractLocation instanceof State && abstractLocation.contains(subsetFromValue)) {
-                        set.add(abstractLocation.getName());
-                    }
+                    foundLocations.add(subsetFromValue);
                 }
+            }
+        }
+        set.put(fieldName, foundLocations);
+    }
+
+    private static void addElementInCountry(String fieldName, String value, Map<String, Set<String>> set) {
+        Set<String> allSubsetsFromValue = getAllSubsetsFromString(value);
+        Set<String> foundLocations = new HashSet<>();
+        for (String subsetFromValue : allSubsetsFromValue) {
+            Set<AbstractLocation> list = new HashSet<>(SolutionUtil.multimap.get(subsetFromValue));
+            for (AbstractLocation abstractLocation : list) {
+                if (abstractLocation == null) {
+                    foundLocations.add(subsetFromValue);
+                }
+                set.put(fieldName, foundLocations);
             }
         }
     }
 
-    private static void addElementInCountry(String value, Set<String> set) {
-        List<AbstractLocation> list = SolutionUtil.multimap.get(value);
-        if (SolutionUtil.multimap.containsKey(value) && list.parallelStream().allMatch(Objects::isNull)) {
-            set.add(value);
-        }
-    }
-
-    public static List<String> getAllSubsetsFromString(String input) {
+    public static Set<String> getAllSubsetsFromString(String input) {
         String[] set = input.trim().split(" ");
-        List<String> subsetList = new ArrayList<>();
+        Set<String> subsetList = new HashSet<>();
         int n = set.length;
 
         for (int i = 0; i < (1 << n); i++) {
@@ -132,7 +184,6 @@ public class Solution {
                 subsetList.add(String.valueOf(subset));
             }
         }
-        Collections.reverse(subsetList);
         return subsetList;
     }
 }
--- src/main/java/application/solution/SolutionLoader.java
+++ src/main/java/application/solution/SolutionLoader.java
@@ -2,33 +2,29 @@ package application.solution;
 
 import application.dataset.storage.DataStorage;
 import application.testData.model.TestObject;
-import application.testData.util.TestUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SolutionLoader {
     public static void main(String[] args) {
-        // System.out.println(Arrays.toString(SolutionUtil.getCanonicalForm(new String[]{"    hakuna MATATA %^$# nº ? &"})));
-
 //        DataStorage.createDataStorage();
 //        DataStorage.saveDataStorage();
 //        SolutionUtil.saveMultimap();
 
-
         DataStorage.loadDataStorage();
         SolutionUtil.loadMultimap();
 
-//        for (var abstractLocation : SolutionUtil.multimap.get("slobozia")) {
-//            System.out.print(abstractLocation.getName());
-//            System.out.println(abstractLocation instanceof State);
-//        }
+//                City: slatina,
+//                State: slatina => slatina, olt. e mai prioritar o resedinta de judet
 
-//        List<AbstractLocation> abstractLocations = SolutionUtil.multimap.get("bucuresti");
-//        for (AbstractLocation abstractLocation : abstractLocations) {
-//            System.out.println(abstractLocation.getName());
-//        }
-
-        List<TestObject> list = TestUtil.readFromSerializedFile("./files/test/correctRandomAddresses/RO.ser");
+        List<TestObject> list = new ArrayList<>(); //TestUtil.readFromSerializedFile("./files/test/correctRandomAddresses/RO.ser");
+//        list.add(new TestObject(null, "slatina", "slatina", null, null, null, "romania"));
+//        list.add(new TestObject(null, "oravita", "caras severin", null, null, null, "romania"));
+        list.add(new TestObject(null, "com bran podoleni", "brasov iasi", null, null, null, "romania iasi"));
+//        List<String> names = new ArrayList<String>();
+//        SolutionUtil.multimap.get("bran").forEach(a -> names.add(a.getName()));
+//        System.out.println(names);
         System.out.println(Solution.getNumberOfCorrectAddressesAfterCorrection(list));
     }
 }
